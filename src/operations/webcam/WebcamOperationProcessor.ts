27cd76c469c122943e15034a2adff4ce6a72e71e
Tristan Bastian <tristan.bastian@softwareag.com>
2023-02-27 23:40:34 +0100

    adjust debug option

--- src/operations/webcam/WebcamOperationProcessor.ts
+++ src/operations/webcam/WebcamOperationProcessor.ts
@@ -125,7 +125,7 @@ export class WebcamOperationProcessor extends OperationProcessor {
         nodeIntegration: true,
         preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
       },
-      show: process.argv.includes("--debug"),
+      show: process.argv.includes("--debugBrowser"),
       width: 800,
     });
 
